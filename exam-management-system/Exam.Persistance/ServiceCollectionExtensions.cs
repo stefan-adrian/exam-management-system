33989c389491c875419e7307b942346499f00a72
narcisdiaconu <narcis.diaconu@info.uaic.ro>
2018-12-09 18:24:32 +0200

    Persistance - Add To Services

---
+++ exam-management-system/Exam.Persistance/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Exam.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Exam.Persistance
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ExamContext>(opt =>
+                opt.UseSqlServer(connectionString));
+
+            services.AddScoped<IReadRepository>(provider => provider.GetService<ExamContext>());
+            services.AddScoped<IWriteRepository>(provider => provider.GetService<ExamContext>());
+
+            return services;
+        }
+    }
+}
