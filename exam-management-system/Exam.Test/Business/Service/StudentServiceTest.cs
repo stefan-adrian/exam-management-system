76ee79dc44077f5865a81183ae134ac21a85bf83
Alex Mihaes <alex.mihaes@protonmail.com>
2018-12-14 22:51:16 +0200

    GetAll_ShouldReturnAllStudents

--- exam-management-system/Exam.Test/Business/Service/StudentServiceTest.cs
+++ exam-management-system/Exam.Test/Business/Service/StudentServiceTest.cs
@@ -8,6 +8,9 @@ using Exam.Test.TestUtils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using MockQueryable.NSubstitute;
+using Nito.AsyncEx;
 
 
 namespace Exam.Test.Business.Service
@@ -16,11 +19,15 @@ namespace Exam.Test.Business.Service
     public class StudentServiceTest
     {
         private Domain.Entities.Student _student1, _student2;
+
         private StudentDetailsDto _studentDto1, _studentDto2;
+
         // mocks
         private Mock<IReadRepository> _mockReadRepository;
         private Mock<IWriteRepository> _mockWriteRepository;
         private Mock<IStudentMapper> _mockStudentMapper;
+        // injectMocks
+        private StudentService _studentService;
 
         [TestInitialize]
         public void TestInitialize()
@@ -32,7 +39,8 @@ namespace Exam.Test.Business.Service
             this._mockReadRepository = new Mock<IReadRepository>();
             this._mockWriteRepository = new Mock<IWriteRepository>();
             this._mockStudentMapper = new Mock<IStudentMapper>();
-
+            _studentService = new StudentService(_mockReadRepository.Object, _mockWriteRepository.Object,
+                _mockStudentMapper.Object);
         }
 
         [TestMethod]
@@ -40,18 +48,31 @@ namespace Exam.Test.Business.Service
         {
             // Arrange
             var expectedStudentsDtoList = new List<StudentDetailsDto> {_studentDto1, _studentDto2};
-            var expectedStudentsList = new List<Student> {_student1, _student2};
-            var expectedStudents = expectedStudentsList.AsQueryable();
-            var studentService = new StudentService(_mockReadRepository.Object, _mockWriteRepository.Object, _mockStudentMapper.Object);
-            _mockReadRepository.Setup(repo => repo.GetAll<Student>()).Returns(expectedStudents);
+            var studentsList = new List<Student> {_student1, _student2};
+            var mockStudentsQueryable = studentsList.AsQueryable().BuildMock();
+            _mockReadRepository.Setup(repo => repo.GetAll<Student>()).Returns(mockStudentsQueryable);
             _mockStudentMapper.Setup(student => student.Map(_student1)).Returns(_studentDto1);
-            _mockStudentMapper.Setup(student => student.Map(_student2)).Returns(_studentDto1);
-
+            _mockStudentMapper.Setup(student => student.Map(_student2)).Returns(_studentDto2);
             // Act
-            List<StudentDetailsDto> actualStudentsDtoList = await studentService.GetAll();
-
+            var actualStudentsDtoList = await _studentService.GetAll();
             // Assert
             actualStudentsDtoList.Should().BeEquivalentTo(expectedStudentsDtoList);
+        }
+
+        [TestMethod]
+        public async Task GetById_ShouldReturnAllStudents()
+        {
+            // Arrange
+            var studentService = new StudentService(_mockReadRepository.Object, _mockWriteRepository.Object,
+                _mockStudentMapper.Object);
+            _mockReadRepository.Setup(repo => repo.GetByIdAsync<Student>(_student1.Id)).ReturnsAsync(_student1);
+            _mockStudentMapper.Setup(student => student.Map(_student1)).Returns(_studentDto1);
+
+            // Act
+            StudentDetailsDto actualStudent = await studentService.GetById(_student1.Id);
+
+            // Assert
+            actualStudent.Should().BeEquivalentTo(_studentDto1);
         }
     }
 }
