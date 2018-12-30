cd05d1d8005a107cf19fb5dc563e3095db9ccab1
narcisdiaconu <narcis.diaconu@info.uaic.ro>
2018-12-30 13:38:35 +0200

    StudentCourseTestUtils & StudentCourseMapperTests

---
+++ exam-management-system/Exam.Test/Business/Mapper/StudentCourseMapperTests.cs
@@ -0,0 +1,59 @@
+﻿using Exam.Business.StudentCourse;
+using Exam.Domain.Entities;
+using Exam.Test.TestUtils;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exam.Test.Business.Mapper
+{
+    [TestClass]
+    public class StudentCourseMapperTests
+    {
+        private StudentCourse _studentCourse;
+        private StudentCourseDetailsDto _studentCourseDetailsDto;
+        private StudentCourseCreationDto _studentCourseCreationDto;
+        private IStudentCourseMapper _studentCourseMapper;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this._studentCourse = StudentCourseTestUtils.GetStudentCourse();
+            this._studentCourseCreationDto =
+                StudentCourseTestUtils.GetStudentCourseCreationDto(this._studentCourse.CourseId);
+            this._studentCourseDetailsDto =
+                StudentCourseTestUtils.GetStudentCourseDetailsDto(this._studentCourse.StudentId,
+                    this._studentCourse.CourseId);
+            this._studentCourseMapper = new StudentCourseMapper();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this._studentCourse = null;
+            this._studentCourseCreationDto = null;
+            this._studentCourseDetailsDto = null;
+            this._studentCourseMapper = null;
+        }
+
+        [TestMethod]
+        public void Map_ShouldReturnStudentCourse_WhenArgumentsAreStudentIdAndStudentCourseCreationDto()
+        {
+            // Act
+            var result = this._studentCourseMapper.Map(this._studentCourse.StudentId, this._studentCourseCreationDto);
+            // Assert
+            result.Should().Match<StudentCourse>((obj) =>
+                obj.CourseId == this._studentCourse.CourseId &&
+                obj.StudentId == this._studentCourse.StudentId);
+        }
+
+        [TestMethod]
+        public void Map_ShouldReturnStudentCourseDetailsDto_WhenArgumentIsStudentCourse()
+        {
+            // Act
+            var result = this._studentCourseMapper.Map(this._studentCourse);
+            // Assert
+            result.Should().BeEquivalentTo(this._studentCourseDetailsDto);
+        }
+
+    }
+}
---
+++ exam-management-system/Exam.Test/TestUtils/StudentCourseTestUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Exam.Business.StudentCourse;
+using Exam.Domain.Entities;
+
+namespace Exam.Test.TestUtils
+{
+    public class StudentCourseTestUtils
+    {
+        private static StudentCourse studentCourse1 = null;
+        private static StudentCourse studentCourse2 = null;
+
+        public static StudentCourse GetStudentCourse()
+        {
+            if (studentCourse1 == null)
+            {
+                studentCourse1 = new StudentCourse(Guid.NewGuid(), Guid.NewGuid());
+            }
+
+            return studentCourse1;
+        }
+
+        public static StudentCourse GetStudentCourse2()
+        {
+            if (studentCourse2 == null)
+            {
+                studentCourse2 = new StudentCourse(Guid.NewGuid(), Guid.NewGuid());
+            }
+
+            return studentCourse2;
+        }
+
+        public static StudentCourseDetailsDto GetStudentCourseDetailsDto(Guid studentId, Guid courseId)
+        {
+            return new StudentCourseDetailsDto
+            {
+                StudentId = studentId,
+                Student = null,
+                CourseId = courseId,
+                Course = null
+            };
+        }
+
+        public static StudentCourseCreationDto GetStudentCourseCreationDto(Guid courseId)
+        {
+            return new StudentCourseCreationDto
+            {
+                CourseId = courseId
+            };
+        }
+    }
+}
