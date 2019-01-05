10ec58bde5807e8d3e2a9f36ddd6831c1b8f46ec
Alex Mihaes <alex.mihaes@protonmail.com>
2019-01-05 23:50:39 +0200

    Refactoring

--- exam-management-system/Exam.Business/Student/Dto/StudentCreationDto.cs
+++ exam-management-system/Exam.Business/Student/Dto/StudentCreationDto.cs
@@ -5,22 +5,21 @@ namespace Exam.Business.Student
     public class StudentCreationDto
     {
         [Required]
+        public string RegistrationNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
 
         [Required]
         public string LastName { get; set; }
 
         [Required]
-        public string Email { get; set; }
-
-        [Required]
-        public string RegistrationNumber { get; set; }
-
-        [Required]
         public int YearOfStudy { get; set; }
-
-        [Required]
-        public string Password { get; set; }
-
     }
 }
